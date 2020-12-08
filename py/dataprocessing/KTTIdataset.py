fe1d419f804465e5c26a60ed394fa4df10dfd6de
wyz <122500997@qq.com>
2020-12-08 07:45:15 +0000

    add class KTTIFragmentDataset

--- py/dataprocessing/KTTIdataset.py
+++ py/dataprocessing/KTTIdataset.py
@@ -6,6 +6,7 @@
 # functions:
 #    collate_to_list: dataloader组合函数。
 # -----------------------------------------------------------------
+from types import DynamicClassAttribute
 import torch
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -17,6 +18,8 @@ from torch.utils.data import DataLoader, Dataset
 from collections import Iterable
 from torchvision import transforms
 import pandas
+
+from py.visualization.box_ops import boxes_value_to_percentage
 
 #%%
 def get_targets_from_files(targets_path, exclusive_raws={}, exclusive_cats=[]):
@@ -41,7 +44,7 @@ class KTTIDataset(Dataset):
     创建一个KTTI dataset
     """
     type_id =  ['Car', 'Cyclist', 'Misc', 'Pedestrian', 'Person', 'Tram', 'Truck', 'Van'] 
-    def __init__(self, path, img_path, return_frame=True,  exclusive_raws={}, exclusive_cats=[], img_transformer=None, target_transformer=None, transformers=None):
+    def __init__(self, path, img_path, return_frame=True,  exclusive_raws={}, exclusive_cats=[], img_transformer=None, target_transformer=None, im_target_trans=None):
         """
         input:
             path: <string> path of label files(*.txt).
@@ -58,6 +61,7 @@ class KTTIDataset(Dataset):
         self.imgs_path = img_path
         self.img_transformer = img_transformer
         self.target_transformer = target_transformer
+        self.im_target_trans = im_target_trans
         if 'type' in exclusive_raws:
             for item in exclusive_raws['type']:
                 if item in KTTIDataset.type_id:
@@ -81,6 +85,7 @@ class KTTIDataset(Dataset):
         self.num = [len(v) for k, v in self.all_targets_dict.items()]
         if return_frame is True:
             self.all_targets_dict = [v for k, v in self.all_targets_dict.items()]
+            self.lens = [len(v) for v in self.all_targets_dict]
             self.all_targets_dict = [x for l in self.all_targets_dict for x in l]
 
     def __getitem__(self, idx):
@@ -105,12 +110,42 @@ class KTTIDataset(Dataset):
                 target = self.target_transformer(target)
             if self.img_transformer is not None:
                 img = self.img_transformer(img)
+            if self.im_target_trans is not None:
+                img, target = self.im_target_trans(img, target)
         else:
             raise ValueError('The function to return a entire sequence is not yet implemented.')
         return img, target
 
     def __len__(self):
         return sum(self.num)
+
+class KTTIFragmentDataset(KTTIDataset):
+    def __init__(self, path, img_path, len, exclusive_raws={}, exclusive_cats={}, img_transformer=None, target_transformer=None, im_target_trans=None):
+        self.len = len
+        super().__init__(path, img_path, return_frame=True, exclusive_raws=exclusive_raws, 
+                        exclusive_cats=exclusive_cats, img_transformer=img_transformer, 
+                        target_transformer=target_transformer, im_target_trans=im_target_trans)
+
+        masks = [np.ones(len, dtype=np.int) for len in self.lens]
+        for mask in masks:
+            mask[-(self.len - 1):] = 0 
+        masks = np.concatenate(masks, axis=0)    
+        self.index = np.where(masks > 0)[0]
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, idx):
+        idx = self.index[idx]  # 获取index 避开长度不够的地方
+        imgs = []
+        targets = []
+        for i in range(self.len):
+            img, target = super().__getitem__(idx)
+            imgs.append(img)
+            targets.append(target)
+        return imgs, targets
+
+
 
 def collate_to_list(batch):
     return list(zip(*batch))
@@ -123,7 +158,10 @@ class CropAnns:
         target = {}
         for name in anns:
             if name in self.name:
-                target[name] = anns[name]
+                if name == 'fragment':
+                    target[name] = int(anns[name])
+                else:
+                    target[name] = anns[name]
         return target
 
 class ToTensor:
@@ -136,14 +174,68 @@ class ToTensor:
             target[name] = torch.tensor(anns[name])
         return target
 
+class ToPercentage:
+    def __init__(self):
+        pass
+
+    def __call__(self, imgs, anns):
+        h, w = imgs.shape[-2:]
+        target = {}
+        for name in anns:
+            if name == 'boxes':
+                target[name] = boxes_value_to_percentage(anns[name], (h, w))
+            else:
+                target[name] = anns[name]
+        return imgs, target
+
+class CovertBoxes:
+    def __init__(self, orig_form, target_form):
+        self.orig_form = orig_form
+        self.target_form = target_form
+
+    def __call__(self, imgs, anns):
+        target = {}
+        for name in anns:
+            if name == 'boxes':
+                target[name] = torchvision.ops.box_convert(
+                        anns[name], 
+                        self.orig_form, 
+                        self.target_form
+                    )
+            else:
+                target[name] = anns[name]
+        return imgs, target
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            format_string += "    {0}".format(t)
+        format_string += "\n)"
+        return format_string
+
 def build_KTTIDataset():
     img_transformer = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),       
     ])
     target_transformer = transforms.Compose([
-        CropAnns(['boxes', 'labels', 'size', 'orig_size']),
+        CropAnns(['boxes', 'labels', 'size', 'orig_size', 'fragment', 'frame', 'track_id']),
         ToTensor()
+    ])
+    im_target_trans = Compose([
+        ToPercentage(),
+        CovertBoxes('xyxy', 'cxcywh')
     ])
     
     ds = KTTIDataset(
@@ -152,28 +244,38 @@ def build_KTTIDataset():
         exclusive_raws={'track_id':[-1], 'type':['Misc', 'Cyclist', 'Person', 'Tram', 'Truck', 'Van']}, 
         exclusive_cats=['dimensions_0', 'dimensions_1', 'dimensions_2'],
         img_transformer=img_transformer,
-        target_transformer=target_transformer
+        target_transformer=target_transformer,
+        im_target_trans=im_target_trans
         )
     return ds
 
-# %%
-if __name__ =='__main__':
+def build_KTTIFragmentDataset():
     img_transformer = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),       
     ])
     target_transformer = transforms.Compose([
-        CropAnns(['boxes', 'labels', 'size', 'orig_size']),
+        CropAnns(['boxes', 'labels', 'size', 'orig_size', 'fragment', 'frame', 'track_id']),
         ToTensor()
     ])
-    
-    ds = KTTIDataset(
+    im_target_trans = Compose([
+        ToPercentage(),
+        CovertBoxes('xyxy', 'cxcywh')
+    ])
+    ds = KTTIFragmentDataset(
         path='/share/data/KTTI/training/label_02', 
         img_path='/share/data/KTTI/trackong_image/training/image_02', 
+        len=3,
         exclusive_raws={'track_id':[-1], 'type':['Misc', 'Cyclist', 'Person', 'Tram', 'Truck', 'Van']}, 
         exclusive_cats=['dimensions_0', 'dimensions_1', 'dimensions_2'],
         img_transformer=img_transformer,
-        target_transformer=target_transformer
-        )
+        target_transformer=target_transformer,
+        im_target_trans=im_target_trans
+    )
+    return ds
+
+# %%
+if __name__ =='__main__':
+    ds = build_KTTIDataset()
     train_dl = DataLoader(ds, batch_size=3, shuffle=True, collate_fn=collate_to_list)
-    next(iter(train_dl))
+    print(next(iter(train_dl)))
